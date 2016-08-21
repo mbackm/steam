f3c5228e88d3ea82fc45c9f9b8b30e884888f807
suvidev <suvi.devteam@gmail.com>
2016-08-21 10:09:19 +0700

    Update HV_Reloader_Mage.user.js

--- HV_Reloader_Mage.user.js
+++ HV_Reloader_Mage.user.js
@@ -4,7 +4,7 @@
 // @author      nihilvoid, Dan31, FabulousCupcake, ??
 // @run-at      document-end
 // @include     /^https?:\/\/(alt|www)?\.?hentaiverse\.org.*$/
-// @version     1.3.3.18
+// @version     1.3.3.19
 // @updateURL       https://github.com/suvidev/hv/raw/master/HV_Reloader_Mage.user.js
 // @downloadURL     https://github.com/suvidev/hv/raw/master/HV_Reloader_Mage.user.js
 // @grant       none
@@ -2140,6 +2140,12 @@ function AI() {
 
 			if(!checkForBuff('spark of life')){
 				if(castSpell('spark of life',0)){
+					return;
+				}
+			}
+
+			if(!checkForBuff('absorbing ward')){
+				if(castSpell('absorbing ward',0)){
 					return;
 				}
 			}
