83706dbc0d11e4b5251442713de22ce47c433225
suvidev <suvi.devteam@gmail.com>
2016-09-04 13:47:04 +0700

    Update HV_Reloader_Mage.user.js

--- HV_Reloader_Mage.user.js
+++ HV_Reloader_Mage.user.js
@@ -4,7 +4,7 @@
 // @author      nihilvoid, Dan31, FabulousCupcake, ??
 // @run-at      document-end
 // @include     /^https?:\/\/(alt|www)?\.?hentaiverse\.org.*$/
-// @version     1.3.3.54
+// @version     1.3.3.55
 // @updateURL       https://github.com/suvidev/hv/raw/master/HV_Reloader_Mage.user.js
 // @downloadURL     https://github.com/suvidev/hv/raw/master/HV_Reloader_Mage.user.js
 // @grant       none
@@ -2210,7 +2210,7 @@ function OnPageReload() {
                 if (STYLE == 'mage') {
                     MAINTAIN_CHANNELING_BUFFS.push('arcane focus');
                     //MAINTAIN_CHANNELING_BUFFS.push('absorbing ward');
-                    lowerHPAlert = 45;
+                    lowerHPAlert = 35;
                 } else {
                     MAINTAIN_CHANNELING_BUFFS.push('heartseeker');
                 }
