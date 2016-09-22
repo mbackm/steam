511f60840a4a9dc7b365c5227365de5691b46ff7
suvidev <suvi.devteam@gmail.com>
2016-09-22 15:54:20 +0700

    Update HV_Reloader_Melee_1H.user.js

--- HV_Reloader_Melee_1H.user.js
+++ HV_Reloader_Melee_1H.user.js
@@ -2367,6 +2367,11 @@ function OnPageReload() {
 								localStorage.setItem("listBTJson", JSON.stringify(listBTJson));
 								localStorage.setItem("BattleStateExHistorySave", false);
 							}
+
+							if(listKindex === -1){
+								localStorage.removeItem("BattleStateExTrackDropLogs")
+								localStorage.setItem("BattleStateExTrackDropOtherEQ", 0);
+							}
 						}// end track battle stats history
 					}
 				}
